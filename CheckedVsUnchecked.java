98dc52b6fc5631fbf1a82385fe45d2d4086268df
Rajat Gupta <imrajatgupta@gmail.com>
2018-04-30 21:01:19 +0530

    Feat: Exceptions, errors and handling

---
+++ CheckedVsUnchecked.java
@@ -0,0 +1,15 @@
+public class CheckedVsUnchecked {
+
+    public static void main(String[] args) throws Exception {
+        myMethod();
+//        anotherMethod();
+    }
+
+    public static void myMethod() throws Exception {
+        throw new Exception("Failed to load");
+    }
+
+    public static void anotherMethod() throws RuntimeException {
+        throw new RuntimeException("Wrong parameter");
+    }
+}
---
+++ UnderstandingExceptions.java
@@ -0,0 +1,6 @@
+public class UnderstandingExceptions {
+    public static void main(String[] args) {
+        int[] myArray = new int[2];
+        System.out.println(myArray[5]);
+    }
+}

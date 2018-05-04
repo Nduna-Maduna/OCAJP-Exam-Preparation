da2abc009e8690f7de0fd67d0946821ab0ccee82
Rajat Gupta <imrajatgupta@gmail.com>
2018-05-04 15:46:37 +0530

    Update: Examples of String methods, String Builder

---
+++ StringImmutability.java
@@ -0,0 +1,22 @@
+public class StringImmutability {
+
+    public static void main(String[] args) {
+        String hello = "hello";
+        String hi = hello + "world"; // hi="helloworld"
+        hi = hello; // hi="hello"
+
+        System.out.println(hi + hello); // hellohello
+
+        hello = hello.toUpperCase();
+
+        System.out.println(hello);
+
+        // concat
+        String s1 = "1";
+        String s2 = s1.concat("2"); // 12
+        s2.concat("3");
+
+        System.out.println(s1); //1
+        System.out.println(s2); //12
+    }
+}

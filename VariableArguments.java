86f94ed58107d69197d35fc99b20da0e13e92b08
Rajat Gupta <imrajatgupta@gmail.com>
2018-05-08 19:37:17 +0530

    feat: Work with Arrays, break, continue statements

---
+++ VariableArguments.java
@@ -0,0 +1,21 @@
+
+public class VariableArguments {
+
+//    public static void main(String[] args) {
+//        System.out.println(args.length);
+//        System.out.println(Arrays.toString(args));
+//    }
+
+    public static void main(String... args) {
+        System.out.println(args.length);
+
+        print(args);
+        print("Java");
+        print("this", "is", "my", "string", "array");
+        print("variable", "arguments", "are", "nice", "and", "useful", "feature");
+    }
+
+    private static void print(String... myArguments) {
+        System.out.println(Arrays.toString(myArguments));
+    }
+}
